695334083bb65658d13078086ba3b09071d88143
Delouya1 <delouya1@gmail.com>
2023-05-22 16:43:28 +0300

    Update GuestHandler.java

--- src/Scrabble/Model/GuestHandler.java
+++ src/Scrabble/Model/GuestHandler.java
@@ -4,8 +4,12 @@ import Scrabble.server.ClientHandler;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class GuestHandler implements ClientHandler {
+    Scanner in;
+    PrintWriter out;
     @Override
     public void handleClient(InputStream inFromclient, OutputStream outToClient) {
 
